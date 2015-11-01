62d0fe591c262812716e5f161f02a0c3b21ec59b
frankdenggx <frankdenggx@163.com>
2015-11-01 16:34:49 +0800

    create master

---
+++ gittestpro/doc/hozdo.sql
@@ -0,0 +1,6 @@
+/**
+ * hozdo.sql
+ */
+CREATE TABLE hozdo(
+	name VARCHAR(255) DEFAULT NULL
+);
---
+++ gittestpro/src/Hozdo.java
@@ -0,0 +1,13 @@
+public class Hozdo {
+	
+	private static final _HOZDO_NAME = "HOZDO Logisitics Group.";
+	
+	private String name = _HOZDO_NAME;
+	
+	public Hozdo(String name) {
+		if (name != null) {
+			this.name = name;
+		}
+	}
+	
+}
